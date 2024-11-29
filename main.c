8f615925b5c30b4a6df3b3cc9b914a7cb0ea8ebd
Sen Harald Hjort Inao <senharald07@gmail.com>
2024-11-29 21:44:20 +0100

    created keystroke handler

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
 #include <windows.h>
+#include <stdio.h>
+
+#define WIDTH 500
+#define HEIGHT 500
+#define PADDLE_WIDTH 20
+#define PADDLE_HEIGHT 100
+#define PADDLE_OFFSET 25
+#define PADDLE_SPEED 10
+
+typedef struct Paddle {
+  int x;
+  int y;
+} Paddle;
+
+Paddle paddle1 = {PADDLE_OFFSET, 0};
+Paddle paddle2 = {WIDTH - PADDLE_OFFSET, 0};
 
 LRESULT WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   switch (uMsg) {
@@ -9,10 +25,11 @@ LRESULT WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
       HBRUSH white_brush = CreateSolidBrush(RGB(255,255,255));
 
       RECT pong1 = {
-        500, 210, 520, 315
+        paddle1.x, paddle1.y, paddle1.x+PADDLE_WIDTH, paddle1.y+PADDLE_HEIGHT
       };
+
       RECT pong2 = {
-        30, 210, 50, 315
+        paddle2.x, paddle2.y, paddle2.x+PADDLE_WIDTH, paddle2.y+PADDLE_HEIGHT
       };
 
       FillRect(hDc, &ps.rcPaint, black_brush);
@@ -22,6 +39,26 @@ LRESULT WinProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
       ReleaseDC(hWnd, hDc);
 
       EndPaint(hWnd, &ps);
+      return 0;
+    }
+
+    case WM_TIMER: {
+      paddle1.y+=PADDLE_SPEED;
+      InvalidateRect(hWnd, NULL, 1);
+      return 0;
+    }
+
+    case WM_KEYDOWN: {
+      switch (LOWORD(wParam))
+      {
+        case 40:
+          paddle2.y+=PADDLE_SPEED;
+          break;
+        case 38:
+          paddle2.y-=PADDLE_SPEED;
+          break;
+      }
+      InvalidateRect(hWnd, NULL, 1);
       return 0;
     }
 
@@ -50,9 +87,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     "NirsClass"
   };
 
+
   RegisterClassA(&class);
 
   HWND windowHandle = CreateWindow("NirsClass", "Pong", WS_CAPTION | WS_POPUP | WS_SYSMENU, 50, 50, 550, 550, NULL, NULL, hInstance, NULL);
+  SetTimer(windowHandle, 0, 1000, NULL);
+
   ShowWindow(windowHandle, nShowCmd);
 
   MSG msg;
